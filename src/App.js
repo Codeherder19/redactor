81ae94b42ba0ff5d2417000dc386e2d907044839
Brian Forbes <brianandrewf81@gmail.com>
2021-06-07 00:54:13 -0600

    Add conditional rendering based on whether or not there is state for this.state.redactedDoc

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 import Form from './Form.js'
+import {Component} from 'react'
+import RedactedDoc from './RedactedDoc'
 
-const App = () => {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      redactedDoc: '',
+    };
+  }
 
+  updateDoc = (event, value) => {
+    event.preventDefault()
+    this.setState({redactedDoc: value});
+}
+
+  render() {
     return (
-    <div className="App">
-      <header>
-        <h1>REDACTOR</h1>
-      </header>
-      <Form />
-    </div>
-  );
+      <div className="App">
+        <header>
+          <h1>REDACTOR</h1>
+        </header>
+        {!this.state.redactedDoc &&
+        <Form updateDoc={this.updateDoc}/>}
+        {this.state.redactedDoc &&
+          <>
+        <RedactedDoc text={this.state.redactedDoc}/>
+        <button onClick={() => this.setState({redactedDoc: ''})}>Reset</button>
+        </>
+        }
+      </div>
+    );
+  }
 }
 
 
